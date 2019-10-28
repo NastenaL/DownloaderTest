34952812364b588f66efb00838e91e57a5d35844
Настя Настя <8ipoet8@gmail.com>
2019-10-28 23:31:13 +0300

    Добавила userControls для раздела "Очередь"

---
+++ DownLoader/Controls/MainPage/QueueAddControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace DownLoader.Controls.MainPage
+{
+    public sealed partial class QueueAddControl : UserControl
+    {
+        public QueueAddControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ClosePopUp(object sender, RoutedEventArgs e)
+        {
+            Popup p = (Popup)this.Parent;
+
+            p.IsOpen = false;
+        }
+    }
+}
---
+++ DownLoader/Controls/MainPage/QueueControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace DownLoader.Controls.MainPage
+{
+    public sealed partial class QueueControl : UserControl
+    {
+        public QueueControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ClosePopUp(object sender, RoutedEventArgs e)
+        {
+            Popup p = (Popup)this.Parent;
+
+            p.IsOpen = false;
+        }
+       
+    }
+}
